e2870d94ca6afe4eb0f3334f78ea1a2203b45b1f
mzakl <90706293+mzakl@users.noreply.github.com>
2025-09-07 09:28:35 +1000

    Update string-manipulation-files.js

---
+++ string-manipulation-files.js
@@ -0,0 +1,8 @@
+let myString = "This is a string";
+let anotherString = "   Another string";
+let hello = "Hello there!";
+let myName = "Mohamad";
+console.log(myString);
+console.log(anotherString);
+console.log(hello);
+console.log(myName);
