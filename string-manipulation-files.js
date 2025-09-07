727304fc218884048d23a47b5ee90244a6d8d07d
mzakl <90706293+mzakl@users.noreply.github.com>
2025-09-07 14:10:45 +1000

    Update string-manipulation-files.js

--- string-manipulation-files.js
+++ string-manipulation-files.js
@@ -2,7 +2,14 @@ let myString = "This is a string";
 let anotherString = "   Another string";
 let hello = "Hello there!";
 let myName = "Mohamad";
-console.log(myString);
-console.log(anotherString);
-console.log(hello);
-console.log(myName);
+
+// Display the value of the defined string variables
+// console.log(myString);
+// console.log(anotherString);
+// console.log(hello);
+// console.log(myName);
+
+// Using utility functions
+console.log(myString.length);
+console.log(myString.charAt(0));
+console.log(myString.charAt(10));
